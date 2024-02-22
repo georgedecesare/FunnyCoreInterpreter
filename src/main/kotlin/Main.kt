d8138e26b1c7a4802a49a69de27fe221bf5ce0ce
George Decesare <86487010+georgedecesare@users.noreply.github.com>
2024-02-22 18:22:25 +0000

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,24 +2,22 @@ import java.io.File
 import kotlin.math.pow
 import kotlin.random.Random
 
-private const val FILE = "division.fc"
+private const val FILE = "test.fc" 
 
 fun main() {
-    test(100)
-}
-
-fun test(n: Int) {
     val core = Core()
     val file = File(FILE)
     val interpreter = Interpreter(file, core)
     interpreter.parse()
-    for (i in 0..n) {
-        val divisor = Random.nextInt(1, 3000)
-        val dividend = Random.nextInt(1, 3000)
-        core[0] = dividend
-        core[1] = divisor
-        interpreter.run()
-        require(core[2] * divisor + core[3] == dividend)
-    }
-    println("Average cycles: ${core.cycles / n}")
+    /*
+    Set registers like this:
+    core[0] = dividend
+    core[1] = divisor
+    */
+    interpreter.run()
+    /*
+    Print registers like this:
+    println(core[2])
+    println(core[3])
+    */
 }
